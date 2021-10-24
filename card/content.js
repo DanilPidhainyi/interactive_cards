2b393abcdda29f493577e196e10473daa980c03b
DanilPidhainyi <danylo.pidgainyi@gmail.com>
2021-10-24 14:21:55 +0300

    add user manual

--- card/content.js
+++ card/content.js
@@ -2,8 +2,8 @@ const upgrade_word = () => {
     $.get(
         '/get_word',
         data => {
-            $('#front').html('<span>' + JSON.parse(data).en + '</span>')
-            $('#back').html('<span>' + JSON.parse(data).uk + '</span>')
+            $('#front').html('<samp class="front__text">' + JSON.parse(data).en + '</samp>')
+            $('#back').html('<span class="back__text">' + JSON.parse(data).uk + '</span>')
         }
     )
 }
